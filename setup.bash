c4282a08bf534e60da6c32b1276ccd906cb9d56d
Casey Marshall <cmars@cmarstech.com>
2014-11-22 20:07:05 -0600

    Add debian wheezy support, using gvm.

--- setup.bash
+++ setup.bash
@@ -1,15 +1,21 @@
 #!/bin/bash -ex
 
-DIST_ID=$(lsb_release -s -i || true)
+DIST_ID=$(bash -c 'lsb_release -s -i | tr [A-Z][a-z]' || true)
 CODENAME=$(lsb_release -s -c || true)
 
 function install_packages {
-	if [[ "$DIST_ID" = "Ubuntu" && -n "$CODENAME" ]]; then
-		package_files=$HOME/dotfiles/ubuntu/$CODENAME/*
+	if [[ -n "$DIST_ID" && -n "$CODENAME" ]]; then
+		package_files=$HOME/dotfiles/$DIST_ID/$CODENAME/*
 		if [ -n "$package_files" ]; then
 			sudo apt-get -y install --no-install-recommends $(grep -h ^[^\#] $package_files | xargs)
 		fi
 	fi
+}
+
+function install_gvm {
+	bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+	source $HOME/.gvm/scripts/gvm
+	gvm use release
 }
 
 # Update packages unless this is CI
@@ -43,8 +49,15 @@ vim +PluginInstall +qall
 # Install more packages
 install_packages
 
+# Install gvm
+install_gvm
+
 # Build and install some useful Go binaries
-$HOME/bin/goinstallpkg
+if hash go 2>/dev/null; then
+	$HOME/bin/goinstallpkg
+else
+	echo "golang not installed"
+fi
 
 # woohoo
 echo "customization complete"
