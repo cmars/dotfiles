1c5f58431e422cec856ef2b4fd0965df39bf2831
Casey Marshall <cmars@cmarstech.com>
2015-03-18 16:33:36 -0500

    Update gvm setup script, go utilities package list, faster gowork cmd.

--- bash/.bash_profile.d/500-gvm.bash
+++ bash/.bash_profile.d/500-gvm.bash
@@ -3,12 +3,14 @@
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the COPYING file for more details.
 
+LATEST=go1.4
+
 if [ -f "$HOME/.gvm/scripts/gvm" ]; then
 	. $HOME/.gvm/scripts/gvm
-	if gvm list | grep release; then
-		gvm use release
+	if gvm list | grep $LATEST; then
+		gvm use $LATEST
 	else
-		echo "go 'release' branch not installed"
-		echo "you need to run: gvm install release"
+		echo "$LATEST not installed"
+		echo "you need to run: gvm install $LATEST"
 	fi
 fi
